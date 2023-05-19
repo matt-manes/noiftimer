83cabf97b8dc330ac46cbf2abe5fec0e30ae981d
matt manes <mattmanes@pm.me>
2023-05-19 13:50:04 -0500

    feat: add prompting and threading for clean shutdown

--- src/noiftimer/stopwatch.py
+++ src/noiftimer/stopwatch.py
@@ -1,18 +1,36 @@
 import time
+import threading
 
 from printbuddies import print_in_place
 
 from noiftimer import Timer
 
+QUIT = False
+
+
+def input_(prompt: str = ""):
+    global QUIT
+    value = input(prompt)
+    if value == "q":
+        QUIT = True
+
+
+def stopwatch():
+    input_thread = threading.Thread(target=input_, daemon=True)
+    input_("Press enter to start. ")
+    if not QUIT:
+        print("Press enter to stop.")
+    timer = Timer(subsecond_resolution=False).start()
+    input_thread.start()
+    while input_thread.is_alive() and not QUIT:
+        print_in_place(f" {timer.elapsed_str} ")
+        time.sleep(1)
+
 
 def main():
-    timer = Timer(subsecond_resolution=False).start()
-    while True:
-        try:
-            print_in_place(f" {timer.elapsed_str}")
-            time.sleep(1)
-        except Exception as e:
-            break
+    print("Press 'q' and then enter to quit at any time.")
+    while not QUIT:
+        stopwatch()
 
 
 if __name__ == "__main__":
