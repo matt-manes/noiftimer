abd843c832c5a789bf186085c28ed1b85b6155e8
matt manes <mattmanes@pm.me>
2023-11-02 13:10:02 -0500

    feat: stopwatch can be paused

--- src/noiftimer/stopwatch.py
+++ src/noiftimer/stopwatch.py
@@ -7,30 +7,45 @@ from noiftimer import Timer
 
 QUIT = False
 
-
-def input_(prompt: str = ""):
-    global QUIT
-    value = input(prompt)
-    if value == "q":
-        QUIT = True
-
-
-def stopwatch():
-    input_thread = threading.Thread(target=input_, daemon=True)
-    input_("Press enter to start. ")
-    if not QUIT:
-        print("Press enter to stop.")
-    timer = Timer(subsecond_resolution=False).start()
-    input_thread.start()
-    while input_thread.is_alive() and not QUIT:
-        print_in_place(f" {timer.elapsed_str} ")
-        time.sleep(1)
-
+class Stopwatch:
+    def __init__(self):
+        self.quit = False
+        self.pause = False
+        self.timer = Timer(subsecond_resolution=False)
+    
+    @property
+    def current_time(self)->str:
+        return f" {self.timer.elapsed_str} "
+    
+    def process_input(self):
+        value = input()
+        if value == "q":
+            self.quit = True
+        elif value == "r":
+            self.timer.reset()
+        elif self.timer.is_paused:
+            self.timer.unpause()
+        else:
+            self.timer.pause()
+    
+    def intro(self):
+        lines = ["","Press enter to pause and unpause the timer.", "Enter 'r' to restart the timer.", "Enter 'q' to quit."]
+        print(*lines, sep="\n")
+    
+    def run(self):
+        input_thread = threading.Thread(target=self.process_input, daemon=True)
+        self.timer.start()
+        input_thread.start()
+        while input_thread.is_alive() and not self.quit:
+            if not self.timer.is_paused:
+                print_in_place(self.current_time)
+            time.sleep(1)
 
 def main():
-    print("Press 'q' and then enter to quit at any time.")
-    while not QUIT:
-        stopwatch()
+    stopwatch = Stopwatch()
+    stopwatch.intro()
+    while not stopwatch.quit:
+        stopwatch.run()
 
 
 if __name__ == "__main__":
